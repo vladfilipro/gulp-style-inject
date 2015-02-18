5aab3cdc70d565c1eaeefbceea8dc52f8415325f
Vlad Filip <vlad.filip@williamhill.com>
2015-02-18 14:04:03 +0100

    done.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var through = require( 'through2' );
 var gutil = require( 'gulp-util' );
+var fs = require( 'fs' );
 
 module.exports = function ( option ) {
     'use strict';
 
-    var CONST_PATTERN = '<\\!--\\s*inject-style\\s*(.*)?\\s*-->';
+    var CONST_PATTERN = '<\\!--\\s*inject-style\\s*(.*?)\\s*-->';
+
+    var self = null;
 
     if ( !option ) {
         option = {};
@@ -12,8 +15,8 @@ module.exports = function ( option ) {
 
     if ( option.match_pattern ) {
         try {
-            new RegExp(option.match_pattern);
-        } catch (e) {
+            new RegExp( option.match_pattern );
+        } catch ( e ) {
             this.emit( 'error',
                 new gutil.PluginError( 'gulp-style-inject', ' Invalid `match_pattern` parameter. Regular expression string required.' ) );
         }
@@ -21,13 +24,40 @@ module.exports = function ( option ) {
         option.match_pattern = CONST_PATTERN;
     }
 
-    if ( typeof option.transform !== 'function' && option.transform ) {
-        this.emit( 'error',
-            new gutil.PluginError( 'gulp-style-inject', 'Invalid transformation in `transform` parameter.' ) );
+    function throwError( msg ) {
+        self.emit( 'error',
+            new gutil.PluginError( 'gulp-style-inject', msg ) );
+    }
+
+    function transformResponse( contents ) {
+        return '<style>\n' + contents + '\n</style>';
+    }
+
+    function getAttributes( params ) {
+        var result = {};
+        var group = params.replace( /\s+/gi, ' ' )
+            .split( ' ' );
+        for ( var i = 0; i < group.length; i++ ) {
+            if ( group[ i ] ) {
+                var combination = group[ i ].split( '=' );
+                result[ combination[ 0 ].replace( /\s*['"](.*)['"]\s*/, '$1' ) ] = combination[ 1 ].replace( /\s*['"](.*)['"]\s*/, '$1' );
+            }
+        }
+        return result;
+    }
+
+    function getStyleFile( source ) {
+        if ( source ) {
+            return transformResponse( fs.readFileSync( source ) );
+        } else {
+            throwError( 'ERROR: No source file specified.' );
+        }
     }
 
     function styleInject( file, enc, callback ) {
         /*jshint validthis:true*/
+
+        self = this;
 
         // Do nothing if no contents
         if ( file.isNull() ) {
@@ -47,19 +77,11 @@ module.exports = function ( option ) {
         if ( file.isBuffer() ) {
             var contents = String( file.contents );
 
-            var occurence = contents.match( new RegExp( option.match, 'gim' ) );
-            if ( occurence.length !== 0 ) {
-                for ( var i = 0; i < occurence.length; i++ ) {
-                    var cssFileMatches = new RegExp( option.match, 'm' ).exec( occurence[ i ] );
-                    if ( !cssFileMatches || !cssFileMatches[ 1 ] ) {
-                        this.emit( 'error',
-                            new gutil.PluginError( 'gulp-style-inject', 'No parameters specified in ' + occurence[ i ] ) );
-                        return callback();
-                    } else {
-                        console.log( cssFileMatches[ 1 ] );
-                    }
-                }
-            }
+            contents = contents.replace( new RegExp( option.match_pattern, 'gi' ), function ( match, parameters ) {
+                var attrs = getAttributes( parameters );
+                return getStyleFile( attrs.src );
+            } );
+
             file.contents = new Buffer( contents );
             this.push( file );
             return callback();
